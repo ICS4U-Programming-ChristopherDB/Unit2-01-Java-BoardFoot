5f05f69d0698530e714fac2e575eb6d647805844
Christopher <92882782+Krostofer@users.noreply.github.com>
2024-03-18 16:59:45 -0400

    Initial commit

---
+++ com/example/BoardFoot.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Christopher Di Bert
+ * @version 1.0
+ * @since 2024-03-18
+ */
+
+// BoardFoot class
+public final class BoardFoot {
+
+    /** Private constructor to prevent instantiation. */
+    private BoardFoot() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
