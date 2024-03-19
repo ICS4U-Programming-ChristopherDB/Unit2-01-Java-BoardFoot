b790fa444390c0dcdad8d90b1c6083786d6ff3dd
Christopher Di Bert <christopher.di-bert@stu.ocsb.ca>
2024-03-19 12:39:49 +0000

    Initial commit

--- com/example/BoardFoot.java
+++ com/example/BoardFoot.java
@@ -1,8 +1,9 @@
-
 package com.example;
 
+import java.util.Scanner;
+
 /**
- * Change me.
+ * This program calculates the length of wood per one board given its width and height.
  *
  * @author Christopher Di Bert
  * @version 1.0
@@ -12,17 +13,52 @@ package com.example;
 // BoardFoot class
 public final class BoardFoot {
 
-    /** Private constructor to prevent instantiation. */
-    private BoardFoot() {
-        throw new UnsupportedOperationException("Cannot instantiate");
-    }
+  /** Private constructor to prevent instantiation. */
+  private BoardFoot() {
+    throw new UnsupportedOperationException("Cannot instantiate");
+  }
 
-    /**
-     * This is the main method.
-     *
-     * @param args Unused
-     */
-    public static void main(final String[] args) {
-        System.out.println("Hello, world");
+  /**
+   * This is the main method.
+   *
+   * @param args Unused
+   */
+  public static void main(final String[] args) {
+    // Instantiate scanner and display welcome message.
+    Scanner sc = new Scanner(System.in);
+    System.out.print("This program finds the length of a wooden board\n");
+    System.out.print("where the volume of one foot is 144in^3 of wood.");
+
+    try {
+      // Gets the height and width of the board from the user.
+      System.out.print("\n\n\n\n\nEnter the height (in): ");
+      double userHeight = sc.nextDouble();
+      System.out.print("Enter the width (in): ");
+      double userWidth = sc.nextDouble();
+
+      // Calls length calculation method.
+      double boardLength = CalculateBoardFoot(userHeight, userWidth);
+      // Outputs the board length to the console.
+      System.out.print("\nThe board length is ");
+      System.out.println(boardLength + " inches.");
+
+      // Error message if invalid input was provided.
+    } catch (Exception e) {
+      System.out.println("You must enter a number!");
     }
+  }
+
+  /**
+   * Method that calculates the length of a board per one board foot.
+   *
+   * @param height passed.
+   * @param width passed.
+   */
+  private static double CalculateBoardFoot(double height, double width) {
+    // Declares the volume of one board length.
+    final double boardVolume = 144f;
+
+    // Calculates and returns the board length.
+    return boardVolume / (height * width);
+  }
 }
